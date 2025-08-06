395d3c4a0f06e08410eb3c436ed5732aa980cb5c
TemitopeAde22 <temitope224468@gmail.com>
2025-08-06 15:28:07 +0100

    Changed Port Number in main.py

--- Backend-app/app/main.py
+++ Backend-app/app/main.py
@@ -53,7 +53,7 @@ origins = [
     "http://localhost:3000",
     "http://localhost:5173",  
     "http://127.0.0.1:5173",  
-    "http://auraflow:80",
+    "http://auraflow:8080",
 ]
 
 app.add_middleware(
